90f30df359dc0429c1f4d03b98eba522ee377f36
liqingyou <742597426@qq.com>
2024-06-13 11:54:11 +0800

    添加测试pwa

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import './assets/scss/main.scss'
 import './assets/scss/iconfont.scss'
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
+import {registerSW} from 'virtual:pwa-register'
+
+// Register the service worker for PWA
+registerSW({immediate: true})
 
 createApp(App).use(router).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,47 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    base: './',
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'My Vue3 PWA',
+                short_name: 'Vue3PWA',
+                description: 'My awesome Vue 3 Progressive Web App!',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'icons/android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'icons/android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                charset: false,
+                additionalData: `@import './src/assets/scss/variable.scss';`
+            }
+        }
     }
-  },
-  css:{
-    preprocessorOptions:{
-      scss:{
-        charset: false,
-        additionalData: `@import './src/assets/scss/variable.scss';`
-      }
-    }
-  }
 })
